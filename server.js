fa81addfbf80496f640803f51703c781cb1eb272
yosbany <ytejas.86@gmail.com>
2025-02-04 14:19:59 -0300

    Actualización del servidor en Render

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const getStock = require("./puppeteerService");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors()); // Habilitar CORS para todas las solicitudes
+app.use(cors()); // Habilitar CORS para permitir solicitudes desde otros dominios
 
+// Ruta base para que no muestre "Cannot GET /"
+app.get("/", (req, res) => {
+    res.send("¡Bienvenido a la API de scraping de Zureo! Usa /stock/{codigo} para obtener el stock.");
+});
+
+// Endpoint para obtener el stock de un artículo
 app.get("/stock/:articleCode", async (req, res) => {
     const { articleCode } = req.params;
     try {
